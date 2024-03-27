460b2f4454cc1472e6028bbb21d85b73cafcc919
devdashk66 <devdashk66@gmail.com>
2024-03-27 15:28:47 +0600

    chenge logo

--- react-movie-db/src/components/common/NavBar.jsx
+++ react-movie-db/src/components/common/NavBar.jsx
@@ -2,19 +2,16 @@ const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <div className="flex gap-2">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Movie DB
           </span>
-        </a>
+        </div>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
